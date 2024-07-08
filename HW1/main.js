e8d22d31b9d32255f770952e7e47473fc7661d1d
Maher Salman <mahersal001@gmail.com>
2024-07-09 01:01:29 +0300

    Add Asset Table

--- HW1/main.js
+++ HW1/main.js
@@ -114,66 +114,164 @@ document.addEventListener('DOMContentLoaded', function () {
     //Assets Table
     const cryptoData = [
         {
-          name: 'USDT',
-          fullName: 'TetherUS',
-          balance: '897.5',
-          averageCost: '$1',
-          todaysPnl: '0%',
-          coinPrice: '$1',
-          logo: 'https://cryptologos.cc/logos/tether-usdt-logo.png?v=022',
-          sparkline: 'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/825.svg'
+            name: 'USDT',
+            fullName: 'TetherUS',
+            balance: '897.5',
+            holdingNetwork: 'Ethereum',
+            averageCost: '$1',
+            todaysPnl: '0%',
+            coinPrice: '$1',
+            logo: 'https://cryptologos.cc/logos/tether-usdt-logo.png?v=022',
+            sparkline: 'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/825.svg'
         },
         {
-          name: 'BTC',
-          fullName: 'Bitcoin',
-          balance: '2',
-          averageCost: '$30,000',
-          todaysPnl: '5%',
-          coinPrice: '$35,000',
-          logo: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=022',
-          sparkline: 'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/1.svg'
+            name: 'BTC',
+            fullName: 'Bitcoin',
+            balance: '2',
+            holdingNetwork: 'Bitcoin',
+            averageCost: '$30,000',
+            todaysPnl: '5%',
+            coinPrice: '$35,000',
+            logo: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=022',
+            sparkline: 'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/1.svg'
+        },
+        {
+            name: 'ETH',
+            fullName: 'Ethereum',
+            balance: '10',
+            holdingNetwork: 'Ethereum',
+            averageCost: '$2,000',
+            todaysPnl: '3%',
+            coinPrice: '$2,200',
+            logo: 'https://cryptologos.cc/logos/ethereum-eth-logo.png?v=022',
+            sparkline: 'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/1027.svg'
+        },
+        {
+            name: 'BNB',
+            fullName: 'Binance Coin',
+            balance: '20',
+            holdingNetwork: 'Binance Smart Chain',
+            averageCost: '$300',
+            todaysPnl: '2%',
+            coinPrice: '$320',
+            logo: 'https://cryptologos.cc/logos/binance-coin-bnb-logo.png?v=022',
+            sparkline: 'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/1839.svg'
+        },
+        {
+            name: 'USDC',
+            fullName: 'USD Coin',
+            balance: '1500',
+            holdingNetwork: 'Ethereum',
+            averageCost: '$1',
+            todaysPnl: '0%',
+            coinPrice: '$1',
+            logo: 'https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=022',
+            sparkline: 'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/3408.svg'
+        },
+        {
+            name: 'XRP',
+            fullName: 'XRP',
+            balance: '5000',
+            holdingNetwork: 'Ripple',
+            averageCost: '$0.5',
+            todaysPnl: '1%',
+            coinPrice: '$0.52',
+            logo: 'https://cryptologos.cc/logos/xrp-xrp-logo.png?v=022',
+            sparkline: 'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/52.svg'
+        },
+        {
+            name: 'ADA',
+            fullName: 'Cardano',
+            balance: '8000',
+            holdingNetwork: 'Cardano',
+            averageCost: '$0.4',
+            todaysPnl: '4%',
+            coinPrice: '$0.42',
+            logo: 'https://cryptologos.cc/logos/cardano-ada-logo.png?v=022',
+            sparkline: 'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/2010.svg'
+        },
+        {
+            name: 'SOL',
+            fullName: 'Solana',
+            balance: '300',
+            holdingNetwork: 'Solana',
+            averageCost: '$40',
+            todaysPnl: '6%',
+            coinPrice: '$45',
+            logo: 'https://cryptologos.cc/logos/solana-sol-logo.png?v=022',
+            sparkline: 'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/5426.svg'
+        },
+        {
+            name: 'DOGE',
+            fullName: 'Dogecoin',
+            balance: '10000',
+            holdingNetwork: 'Dogecoin',
+            averageCost: '$0.1',
+            todaysPnl: '5%',
+            coinPrice: '$0.11',
+            logo: 'https://cryptologos.cc/logos/dogecoin-doge-logo.png?v=022',
+            sparkline: 'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/74.svg'
+        },
+        {
+            name: 'DOT',
+            fullName: 'Polkadot',
+            balance: '1000',
+            holdingNetwork: 'Polkadot',
+            averageCost: '$15',
+            todaysPnl: '4%',
+            coinPrice: '$16',
+            logo: 'https://cryptologos.cc/logos/polkadot-new-dot-logo.png?v=022',
+            sparkline: 'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/6636.svg'
         }
-        // Add more data as needed
-      ];
-  
-      function generateTableRow(data) {
+    ];
+    
+
+    function generateTableRow(data) {
         return `
-          <tr>
-            <td class="px-6 py-4 whitespace-nowrap flex items-center">
-              <img src="${data.logo}" alt="${data.name}" class="h-8 w-8 rounded-full mr-2">
-              <div>
-                <div class="text-lg font-medium text-gray-900 dark:text-white">${data.name}</div>
-                <div class="text-sm text-gray-500 dark:text-gray-400">${data.fullName}</div>
-              </div>
-            </td>
-            <td class="px-6 py-4 whitespace-nowrap">
-              <div class="text-lg text-gray-900 dark:text-white">${data.balance}</div>
-            </td>
-            <td class="px-6 py-4 whitespace-nowrap">
-              <div class="text-lg text-gray-900 dark:text-white">${data.averageCost}</div>
-            </td>
-            <td class="px-6 py-4 whitespace-nowrap">
-              <div class="text-lg text-gray-500 dark:text-white">${data.todaysPnl}</div>
-            </td>
-            <td class="px-6 py-4 whitespace-nowrap">
-              <div class="text-lg text-gray-500 dark:text-white">${data.coinPrice}</div>
-            </td>
-            <td class="px-6 py-4 whitespace-nowrap">
-              <div class="text-lg text-gray-500 dark:text-white">
-                <img src="${data.sparkline}" alt="${data.name}-7d-price-graph" class="h-auto w-auto mr-auto">
-              </div>
-            </td>
-          </tr>
+            <tr>
+                <td class="px-6 py-4 whitespace-nowrap flex items-center">
+                    <img src="${data.logo}" alt="${data.name}" class="h-8 w-8 rounded-full mr-2">
+                    <div>
+                        <div class="text-lg font-medium text-gray-900 dark:text-white">${data.name}</div>
+                        <div class="text-sm text-gray-500 dark:text-gray-400">${data.fullName}</div>
+                    </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-lg text-gray-900 dark:text-white">${data.balance}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-lg text-gray-900 dark:text-white">${data.holdingNetwork}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-lg text-gray-900 dark:text-white">${data.averageCost}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-lg text-gray-500 dark:text-white">${data.todaysPnl}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-lg text-gray-500 dark:text-white">${data.coinPrice}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-lg text-gray-500 dark:text-white">
+                        <img src="${data.sparkline}" alt="${data.name}-7d-price-graph" class="h-auto w-auto mr-auto">
+                    </div>
+                </td>
+            </tr>
         `;
-      }
-  
-      function populateTable() {
+    }
+
+    function populateTable() {
         const tableBody = document.getElementById('cryptoTableBody');
+        const assetTable = document.getElementById('AssetsTable');
+        assetTable.classList.remove('hidden');
+        tableBody.innerHTML = ''; // Clear the table body before populating
         cryptoData.forEach(data => {
-          tableBody.innerHTML += generateTableRow(data);
+            tableBody.innerHTML += generateTableRow(data);
         });
-      }
-  
-      document.addEventListener('DOMContentLoaded', populateTable);    
+    }
+
+    document.getElementById('assetsBtnTable').addEventListener('click', populateTable);
+
   });
   
+
