a6a84044debb15c40dbf1f0fbf9206f316d51b27
Omar Saleh <omarr.saleh19@gmail.com>
2024-08-09 20:11:34 +0300

    homepage1

--- HW2/hw2/src/App.jsx
+++ HW2/hw2/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import NavBar from './components/NavBar';
 import { AppKitProvider } from './AppKitProvider';
 import WalletData from './components/WalletData';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Banner from "./components/Banner";
 
 
@@ -36,25 +35,22 @@ function App() {
   }, [isDarkMode]);
 
   return (
-    <AppKitProvider>
-      <BrowserRouter>
-        <div className={`${isDarkMode ? 'dark' : ''}`}>
-          <NavBar handleConnect={handleConnect} isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
-          <Routes>
-            <Route
-              path="/"
-              element={wallet.isConnected ? (
+    <div>
+        <AppKitProvider>    
+          <NavBar handleConnect={handleConnect} />
+           
+          {wallet.isConnected ? (
+              <div>
                 <WalletData wallet={wallet} />
+              </div>
               ) : (
                 <Banner />
-              )}
-            />
-            <Route path="/wallet" element={<WalletData wallet={wallet} />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </AppKitProvider>
-  );
+              )
+          }
+        </AppKitProvider>
+    </div>
+    );
 }
 
 export default App;
+
