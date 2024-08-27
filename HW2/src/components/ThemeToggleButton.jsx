9e505747afc3274ac06586f48b57b87bfe4441bd
Maher Salman <mahersal001@gmail.com>
2024-08-27 19:50:53 +0300

    edit darkMode Toggle

--- HW2/src/components/ThemeToggleButton.jsx
+++ HW2/src/components/ThemeToggleButton.jsx
@@ -1,17 +1,48 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from './ThemeContext';
+import { ReactSVG } from 'react-svg';
+import SunSVG from '../icon/Sun.svg';
+import MoonSVG from '../icon/Moon.svg';
 
 const ThemeToggleButton = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
+
   return (
-    <button
-      onClick={toggleTheme}
-      className="bg-transparent text-yellow-500 rounded-full hover:bg-yellow-500 hover:text-white w-8 h-8 flex items-center justify-center text-2xl"
+    <div className='mt-[-5] ml-[2.5]'>
+      <input
+        className='sr-only'
+        type='checkbox'
+        id='darkmode-toggle'
+        checked={theme === 'dark'}
+        onChange={toggleTheme}
+      />
+      <label
+        className={`relative block w-16 h-8 rounded-full cursor-pointer transition-all duration-300 shadow-inner ${
+          theme === 'dark' ? 'bg-[#242424]' : 'bg-gray-200'
+        }`}
+        htmlFor='darkmode-toggle'
       >
-      <span role="img" aria-label="sun">
-        {theme === 'light' ? '☀️' : '🌑'}
-      </span>
-    </button>
+        <span
+          className={`absolute top-0.5 left-0.5 w-6 h-6 rounded-full shadow transition-all duration-300 transform ${
+            theme === 'dark'
+              ? 'translate-x-9 bg-gradient-to-b from-gray-400 to-gray-600'
+              : 'bg-gradient-to-b from-[#ffcc89] to-[#d8860b]'
+          }`}
+        />
+        <ReactSVG
+          src={SunSVG}
+          className={`absolute top-1 left-1 w-5 transition-all duration-300 ${
+            theme === 'dark' ? 'text-gray-500' : 'text-white'
+          }`}
+        />
+        <ReactSVG
+          src={MoonSVG}
+          className={`absolute top-1 left-[2.5rem] w-5 transition-all duration-300 ${
+            theme === 'dark' ? 'text-white' : 'text-gray-500'
+          }`}
+        />
+      </label>
+    </div>
   );
 };
 
--- HW2/src/utils/transactionService.js
+++ HW2/src/utils/transactionService.js
@@ -3,6 +3,7 @@ import { getUrlString } from '../utils/urlUtils';
 // Service to fetch transactions from the network
 export const fetchTransactions = async (network, address) => {
   const url = getUrlString(network, 'getAddressHistory', address); // Generate the URL for the request
+  console.log("url" , url);
   const response = await fetch(url); // Make the HTTP request
 
   if (!response.ok) {
